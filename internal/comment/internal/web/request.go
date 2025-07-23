f9f759a8f6598c93807d945068062521d78f849e
codepzj <email@codepzj.cn>
2025-07-23 23:06:16 +0800

    feat: 更新评论请求结构，调整 Url 字段为非必填项，优化数据模型

--- internal/comment/internal/web/request.go
+++ internal/comment/internal/web/request.go
@@ -1,11 +1,11 @@
 package web
 
 type CommentRequest struct {
-	Content   string `json:"content" binding:"required"`
-	Nickname  string `json:"nickname" binding:"required"`
-	Email     string `json:"email" binding:"required"`
-	Url       string `json:"url" binding:"required"`
-	Avatar    string `json:"avatar" binding:"required"`
+	Nickname string `json:"nickname" binding:"required"`
+	Email    string `json:"email" binding:"required"`
+	Url      string `json:"url"`
+	Avatar   string `json:"avatar" binding:"required"`
+	Content  string `json:"content" binding:"required"`
 	PostId    string `json:"post_id" binding:"required,bson_id"`
 	CommentId string `json:"comment_id" binding:"required,bson_id"`
 }
@@ -15,6 +15,6 @@ type CommentEditRequest struct {
 	Content  string `json:"content" binding:"required"`
 	Nickname string `json:"nickname" binding:"required"`
 	Email    string `json:"email" binding:"required"`
-	Url      string `json:"url" binding:"required"`
+	Url      string `json:"url"`
 	Avatar   string `json:"avatar" binding:"required"`
 }
