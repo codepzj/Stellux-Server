5da050cd45a5d8e251743cbfa48c04ff23560d2d
codepzj <email@codepzj.cn>
2025-08-28 23:39:41 +0800

    fix: 更新admin用户密码以增强安全性

--- scripts/mongo-init.sh
+++ scripts/mongo-init.sh
@@ -16,7 +16,7 @@ db.user.insertMany([
     {
         _id: ObjectId("67c453eda04b00c407b431fd"),
         username: "admin",
-        password: "$2a\$10\$SLcnDmaJc1nLtUOsZS4yquXyVeu5E6qJHNTVeKSzTk4JO4Xq/FPSy",
+        password: "$2a\$10\$EPbYKMyDA5RN9AaEEL7RqePI4BotBGCDvZ/ny/YHasEoU4vhU5n4e",
         nickname: "codezj",
         role_id: Int32("0"),
         created_at: ISODate("2025-07-13T02:19:26.865Z"),
